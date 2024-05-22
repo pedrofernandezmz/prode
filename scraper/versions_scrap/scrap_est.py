ce49cb385bc19288e806207f181cda4391a1c5b2
Pedro Fernández <pedrofernandez@MacBook-Air-de-Pedro.local>
2024-05-22 19:27:21 -0300

    tablas mejorads

--- scraper/versions_scrap/scrap_est.py
+++ scraper/versions_scrap/scrap_est.py
@@ -6,13 +6,11 @@ import time
 def scrap_tablas():
     url = "https://www.promiedos.com.ar/primera"
     response = requests.get(url)
-    # VER SI ME CONVIENE USAR TABLE SORTER Y NO INDICES
-    keywords = ["posiciones", "posiciones", "promedios", "promedios2"]
-    indices = [0, 1, 0, 0]
-    names = ["posiciones", "posiciones_anual", "promedios", "promedios_prox"]
+    keywords = ["goleadorest", "goleadorest"]
+    indices = [0, 1]
+    names = ["goleadores", "asistencias"]
     
     if response.status_code == 200:
-
         for keyword, index, name in zip(keywords, indices, names):
             soup = BeautifulSoup(response.content, "html.parser")
             tablas = soup.find_all("table", {"id": keyword})
@@ -21,9 +19,20 @@ def scrap_tablas():
             with open(f"{name}.csv", "w", newline="", encoding="utf-8") as csvfile:
                 writer = csv.writer(csvfile)
                 filas = tabla.find_all("tr")
+                
                 for fila in filas:
                     celdas = fila.find_all(["th", "td"])
-                    writer.writerow([celda.get_text(strip=True) for celda in celdas])
+                    filtered_celdas = []
+                    for i, celda in enumerate(celdas):
+                        texto = celda.get_text(strip=True)
+                        if i == 0:  # Supone que la columna "jugador" es la primera
+                            # Eliminar valores numéricos
+                            texto = ''.join([char for char in texto if not char.isdigit()])
+                            # Eliminar paréntesis y su contenido
+                            texto = texto.replace("(  )", "").strip()
+                        filtered_celdas.append(texto)
+                    writer.writerow(filtered_celdas)
+
             print(f"Los datos se han guardado correctamente en el archivo {name}.csv.")
     else:
         print("Error al realizar la solicitud GET.")
